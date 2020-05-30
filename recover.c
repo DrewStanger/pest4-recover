0c9577bf48e9632614fdb6afc73b8e35655f6aa7
Drew <drewstanger95@gmail.com>
2020-05-30 23:42:33 +0100

    Update recover.c

--- recover.c
+++ recover.c
@@ -7,14 +7,14 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    // THERE MUST BE 1 COMMAND LINE ARG
+    // to check if there is the 1 required command line argument. 
     if (argc != 2)
     {
         fprintf(stderr, "There must be 1 command line argument");
         return 1;
     }
 
-    //OPEN THE MEMORY CARD
+    //Opens the memory card.
     FILE *memcard = fopen(argv[1], "r");
     if (memcard == NULL)
     {
@@ -23,27 +23,27 @@ int main(int argc, char *argv[])
     }
     else
     {
-        //ESTABLISH FILENAME 
+        //allocate memory for the filename 
         char *filename = malloc(8 * sizeof(char));
-             //ESTABLISH THE BUFFER
+        //allocate memory for the buffer (each = 512 bytes).
         BYTE *buffer = malloc(sizeof(BYTE) * 512);
         int fileCounter = 0;
         FILE *img = NULL;
 
-        //WHILE LOOP TO CAPTURE FREAD
+        //This while loop iterates through the file while there are still 512 bytes chunks to read.
         while (fread(buffer, sizeof(BYTE), 512, memcard) == 512)
         {
-            //IF JPEG WRITE TO NEW JPG FILE
+            //When this IF is met a start of a JPEG file is found.
             if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
             {
-               //THIS IS A NEW FILE
+               //If a new file.
                 if (fileCounter > 0)
                 {
                     fclose(img);   
                 }
                     //Make new JPEG file
                     sprintf(filename, "%03i.jpg", fileCounter);
-                    // write to the new JPEG
+                    // write the data to the new JPEG
                     img = fopen(filename, "w");
                     fwrite(buffer, sizeof(BYTE), 512, img);
                     fileCounter++;
@@ -54,6 +54,7 @@ int main(int argc, char *argv[])
             }
             
         }
+        //free the memory previously allocated. to prevent seg errors and memory leaks.
         free(filename);
         free(buffer);
         //close remaining files
